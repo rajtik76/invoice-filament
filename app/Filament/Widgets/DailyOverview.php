0308f183ef3d8a86a24a6b290a4f0707a1af5d95
Vladislav Rajtmajer <rajtik@gmail.com>
2025-03-22 16:26:45 +0100

    Add method to customize table record key in DailyOverview
    Introduced `getTableRecordKey` method to define a custom key for table records, improving flexibility in widget handling. Simplified the query by removing unnecessary `id` and `user_id` selection in `DailyOverview`. These changes enhance maintainability and ensure cleaner data handling.

--- app/Filament/Widgets/DailyOverview.php
+++ app/Filament/Widgets/DailyOverview.php
@@ -7,17 +7,22 @@ use Filament\Support\Colors\Color;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Database\Eloquent\Model;
 
 class DailyOverview extends BaseWidget
 {
     protected int|string|array $columnSpan = 'full';
+
+    public function getTableRecordKey(Model $record): string
+    {
+        return 'DailyOverview';
+    }
 
     public function table(Table $table): Table
     {
         return $table
             ->query(
                 TaskHour::query()
-                    ->select(['id', 'user_id'])
                     ->selectRaw('DATE(date) as day')
                     ->selectRaw('SUM(hours) as sum_hours')
                     ->where('user_id', auth()->id())
